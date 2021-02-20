bfb483ae6e138f1ea9e886c26325d6d3bc083e66
Paul Miller <pdmill@gmail.com>
2021-02-19 16:20:33 -0800

    Created corpus of all top 25 ted talks

---
+++ web/ted_web.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+import numpy as np
+
+st.title('TED Talk Text Generation')
+
+'''
+This app allows you to generate text based on several compiled neural network machine learning models.
+
+The models have been fed a body of text that comprises of the top 25 most viewed TED Talks (through Jan, 2021.)
+         
+'''
